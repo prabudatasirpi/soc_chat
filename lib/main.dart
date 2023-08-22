2978630289c6b71fdcefbbe32315d785aa5d9706
prabudatasirpi <prabuk@datasirpi.com>
2023-08-22 09:27:20 +0530

    browser issues fixed

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,15 @@ import 'package:flutter/material.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+        apiKey: "AIzaSyCmn7Kuo3RkbNbQ8VI48b9Rwc5bGN9LzWQ",
+        authDomain: "socchatapp-8d615.firebaseapp.com",
+        projectId: "socchatapp-8d615",
+        storageBucket: "socchatapp-8d615.appspot.com",
+        messagingSenderId: "1056497732488",
+        appId: "1:1056497732488:web:5abea4749a92897dae483d"),
+  );
   runApp(MyApp());
 }
 
